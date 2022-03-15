43d3c04f50b075b698506b53cdceff771daa9b2f
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2022-03-15 18:33:46 +0400

    version-1.1.1

--- patients/views.py
+++ patients/views.py
@@ -1,11 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import View
 from disease.models import DiseaseModel, OperationModel
 from patients.forms import CreatePatientForm
 from patients.models import PatientModel, PeopleWithPatientModel, BloodTypeModel, GenderModel
 from doctors.models import DoctorModel
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
 import phonenumbers
 # Create your views here.
 
@@ -49,69 +51,49 @@ class PatientPanelView(LoginRequiredMixin,View):
         }
         return render(request,"patient_panel.html",context)
 
-class PatientCreateView(View):
-
-    form = CreatePatientForm
-
-    def get(self, request):
-        gender_choices = GenderModel.objects.all()
-        blood_type_choices = BloodTypeModel.objects.all()
-        diseases = DiseaseModel.objects.all()
-
-        errors = self.form.errors
-
-        context = {
-            "gender_choices":gender_choices,
-            "blood_type_choices":blood_type_choices,
-            "diseases":diseases,
-            # "errors": errors,
-        }
-        return render(request,"create_patient.html",context)
 
 
-    def post(self, request):
-        form = CreatePatientForm(request.POST)
-        print(form.errors)
-    
-        if form.is_valid():
+class PatientCreateView(CreateView):
+    template_name = "create_patient.html"
+    form_class = CreatePatientForm
 
-            first_name = form.cleaned_data.get("first_name")
-            last_name = form.cleaned_data.get("last_name")
-            phonenumber = form.cleaned_data.get("phonenumber")
-            gender = form.cleaned_data.get("gender")
-            blood_type = form.cleaned_data.get("blood_type")
-            born_date = form.cleaned_data.get("born_date")
-            expected_discharging_date = form.cleaned_data.get("expected_discharging_date")
-            diseases = form.cleaned_data.get("disease")
-            additional_information = form.cleaned_data.get("additional_information")
 
-            patient_diseases = DiseaseModel.objects.filter(id__in = diseases)
+    def get_context_data(self,*args, **kwargs):
+        context = super(PatientCreateView, self).get_context_data(*args,**kwargs)
+        context["gender_choices"] = GenderModel.objects.all()
+        context["blood_type_choices"] = BloodTypeModel.objects.all()
+        context["diseases"] = DiseaseModel.objects.all()
+        return context
 
-            try:
-                instance = PatientModel.objects.create(first_name = first_name, last_name = last_name, gender = gender, blood_type = blood_type, born_date = born_date, expected_discharging_date = expected_discharging_date, additional_information = additional_information, is_active = True)
-            except:
-                return self.form_invalid(self, form)
-            instance.disease.add(*patient_diseases)
 
-            try:
-                phonenum = phonenumbers.parse(phonenumber)
-            except phonenumbers.phonenumberutil.NumberParseException:
-                raise ValueError("sehv")
-            
-            else:
-                instance.phonenumber = phonenum
+    def form_valid(self,form, *args, **kwargs):
+        first_name = form.cleaned_data.get("first_name")
+        last_name = form.cleaned_data.get("last_name")
+        phonenumber = form.cleaned_data.get("phonenumber")
+        gender = form.cleaned_data.get("gender")
+        blood_type = form.cleaned_data.get("blood_type")
+        born_date = form.cleaned_data.get("born_date")
+        expected_discharging_date = form.cleaned_data.get("expected_discharging_date")
+        diseases = form.cleaned_data.get("disease")
+        additional_information = form.cleaned_data.get("additional_information")
 
-            return redirect("/")
-        
-        return render(request, "create_patient.html", {'form': form})
-    
+        patient_diseases = DiseaseModel.objects.filter(id__in = diseases)
 
-    def form_invalid(self, request, form, *args, **kwargs):
-        errors = form.errors
-        # context = {
-        #     "errors":errors
-        # }
-        return render(request, "create_patient.html", {'errors': form.errors})
+        try:
+            phonenum = phonenumbers.parse(phonenumber)
+        except:
+            return super().form_invalid(form)
+        else:
+            instance = PatientModel.objects.create(first_name = first_name, last_name = last_name, gender = gender, blood_type = blood_type, born_date = born_date, expected_discharging_date = expected_discharging_date, additional_information = additional_information,phonenumber=phonenumber ,is_active = True)
+
+        instance.disease.add(*patient_diseases)
+
+        return HttpResponseRedirect("patient:panel",instance.id)
+
+
+    def form_invalid(self, form, *args, **kwargs):
+        return super().form_invalid(form)
+        # return render(request, "create_patient.html", {'form': form})   
 
 
 
