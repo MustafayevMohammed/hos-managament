48a251a69762d121ac1c6ea955fba93c853a01d8
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2022-03-16 16:58:32 +0400

    version-1.1.2

--- patients/views.py
+++ patients/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy, reverse
 from django.views import View
 from disease.models import DiseaseModel, OperationModel
@@ -7,7 +7,7 @@ from patients.forms import CreatePatientForm
 from patients.models import PatientModel, PeopleWithPatientModel, BloodTypeModel, GenderModel
 from doctors.models import DoctorModel
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 import phonenumbers
 # Create your views here.
 
@@ -88,7 +88,7 @@ class PatientCreateView(CreateView):
 
         instance.disease.add(*patient_diseases)
 
-        return HttpResponseRedirect("patient:panel",instance.id)
+        return redirect("patient:panel",instance.id)
 
 
     def form_invalid(self, form, *args, **kwargs):
@@ -96,14 +96,64 @@ class PatientCreateView(CreateView):
         # return render(request, "create_patient.html", {'form': form})   
 
 
+class PatientUpdateView(UpdateView):
+    template_name = "patient_edit.html"
+    form_class = CreatePatientForm
+
+    def get_object(self):
+        # id = self.kwargs.get("id")
+        # print(patient)
+        return PatientModel.objects.get(id = self.kwargs.get("id"))
+
+
+    def get_context_data(self,*args, **kwargs):
+        context = super(PatientUpdateView, self).get_context_data(*args,**kwargs)
+        context["gender_choices"] = GenderModel.objects.all()
+        context["blood_type_choices"] = BloodTypeModel.objects.all()
+        context["diseases"] = DiseaseModel.objects.all()
+        context["patient"] = self.get_object
+        patient = PatientModel.objects.get(id = self.kwargs.get("id"))
+        context["patient_diseases"] = patient.disease.all
+        
+        return context
+
+
+    # def form_valid(self, form, *args, **kwargs):
+    #     print(self.get_object)
+    #     first_name = form.cleaned_data.get("first_name")
+    #     last_name = form.cleaned_data.get("last_name")
+    #     phonenumber = form.cleaned_data.get("phonenumber")
+    #     gender = form.cleaned_data.get("gender")
+    #     discharged_date = form.cleaned_data.get("discharged_date")
+    #     born_date = form.cleaned_data.get("born_date")
+    #     blood_type = form.cleaned_data.get("blood_type")
+    #     diseases = form.cleaned_data.get("disease")
+    #     joining_date = form.cleaned_data.get("joining_date")
+    #     expected_discharging_date = form.cleaned_data.get("expected_discharging_date")
+    #     additional_information = form.cleaned_data.get("additional_information")
+
+    #     patient_diseases = DiseaseModel.objects.filter(name__in = diseases)
+    #     print(patient_diseases)
+    #     try:
+    #         phonenum = phonenumbers.parse(phonenumber)
+    #     except:
+    #         return super().form_invalid(form)
+    #     else:
+    #         instance = PatientModel.objects.create(first_name = first_name, last_name = last_name, gender = gender, blood_type = blood_type, born_date = born_date, expected_discharging_date = expected_discharging_date, additional_information = additional_information,phonenumber = phonenumber,discharged_date = discharged_date, joining_date = joining_date, is_active = True)
+        
+    #     instance.disease.add(*patient_diseases)
+    #     return redirect("patient:panel",instance.id)
+    
+    
+    # def form_invalid(self,form, *args, **kwargs):
+    #     return super().form_invalid(form)
+
+    
 
 
 
 def add_patient_status(request,id):
     return render(request,"add_patient_status.html")
-
-def patient_edit(request,id):
-    return render(request,"patient_edit.html")
 
 def ppl_with_patient(request,id):
     return render(request,"add_ppl_with_patient.html")
