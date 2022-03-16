a7111d564111d3471630cc24469e03a338a17561
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2022-03-16 18:39:03 +0400

    version-1.1.3

--- patients/urls.py
+++ patients/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("create/",views.PatientCreateView.as_view(), name = "create"),
     path("list/",views.PatientListView.as_view() ,name = "list"),
     path("panel/<str:id>",views.PatientPanelView.as_view(), name = "panel"),
-    path("edit/<str:id>",views.PatientUpdateView.as_view(), name = "edit"),
-    path("add_status/<str:id>",views.add_patient_status,name = "add_status"),
+    path("edit/<str:id>",views.PatinetEditView.as_view(), name = "edit"),
+    path("add_status/<str:id>",views.AddPatientStatusView.as_view(),name = "add_status"),
     path("add_ppl_with_patient/<str:id>",views.ppl_with_patient,name = "add_ppl_with_patient"),
     path("logs/",views.patient_logs,name = "logs"),
 ]
