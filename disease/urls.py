eef72fb4198319249b5d04e013cf1e09f19108e4
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2022-04-14 15:12:09 +0400

    version-1.8.2

--- disease/urls.py
+++ disease/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("list/",views.OperationListView.as_view(),name = "list"),
     path("create_operation/",views.OperationCreateView.as_view(),name = "create"),
     path("active-deactivate-opration/<str:id>",views.ActivateDeactivateOperation.as_view(),name = "activate_deactivate"),
+    path("active-operations",views.ActiveOperationsListView.as_view(),name = "active_operations"),
 ]
