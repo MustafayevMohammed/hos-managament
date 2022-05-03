7ac2fb374afc2d6c3a05c1f36a8b8d9cb4d8c02a
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2022-05-03 19:30:15 +0400

    version-4.0.0 dockerized and fixed the bug

--- django/core/settings.py
+++ django/core/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG"))) == False
+# DEBUG = bool(int(os.environ.get("DEBUG"))) == False
+DEBUG=True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -92,8 +98,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "pgdb",
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "USER": "AdminUser",
+        "PASSWORD": "mystrongpostgresqlpasswords",
         "HOST": "db",
         "PORT": "5432",
     }
@@ -144,19 +150,25 @@ AUTH_USER_MODEL = "account.CustomUserModel"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     # '/var/www/static/',
 ]
 
-STATIC_ROOT = "/vol/web/static"
+# STATIC_ROOT = "/vol/web/static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = "/vol/web/media"
+MEDIA_URL = "/media/"
+# MEDIA_ROOT = "/vol/web/media"
+
+STATIC_ROOT = "/static/"
+# MEDIA_ROOT = "/opt/services/app/media/"
+print(STATIC_ROOT)
+# print(MEDIA_ROOT)
+
